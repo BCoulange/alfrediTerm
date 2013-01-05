f5f6aa42dfe49e7dbcc6d753bed8de8291bcf1ce
Baptiste Coulange <bc@cornis.fr>
2013-01-05 14:42:41 +0100

    initial commit

---
+++ itermRubyParse.rb
@@ -0,0 +1,37 @@
+require 'ruby-growl'
+require 'ruby-growl/ruby_logo'
+
+$LOAD_PATH << './parsers'
+require 'gitParsing.rb'
+
+# exec the command in terminal
+command = ARGV
+result=`#{command.join(' ')}`
+
+# preparing notif
+g = Growl.new "localhost", "#{command.join(' ')}"
+g.add_notification("notification", "ruby-growl Notification",
+                   Growl::RUBY_LOGO_PNG)
+
+
+# parsing for notifications
+case command[0]
+  when "git"
+  	case command[1]
+  		when "status"
+  			parseGitStatus(result,g)
+  		when "branch"
+  			parseGitBranch(result,g)
+  		when "commit"
+  			parseGitCommit(result,g)
+	end
+   when "subl"
+   	# do nothing
+  else
+  	g.notify "notification", "No alfred rule", "#{result}"
+end
+
+# prompt the result in terminal 
+result.split("\n").each do |line|
+	puts line
+end
---
+++ parsers/gitParsing.rb
@@ -0,0 +1,30 @@
+# git parsing
+def parseGitStatus(result,g)
+	unless result['nothing added to commit'] then
+			modifes=result.split("# Untracked files")[0]
+			modifes=modifes.split("#	")
+			modifes.shift
+			modifes=modifes.map{|m| m.split("\n")[0]}
+			g.notify "notification", "Some changes to commmit!", "#{modifes.join("\n")}"
+	else
+		if result['untracked'] then
+			g.notify "notification", "Untracked files!", "#{result.split("\n")[-1]}"
+		else
+			g.notify "notification", "Nothing to commit!", "#{result.split("\n")[-1]}"
+		end
+	end
+end
+
+def parseGitBranch(result,g)
+	# finding your branch
+	myBranch=result.split("\n").find_all{|item| item.split.include?('*') }
+	# finding others
+	othersBranch=result.split("\n").find_all{|item| !item.split.include?('*') }
+	# notify
+	g.notify "notification", "On branch #{myBranch[0].split('*')[1]}", "#{othersBranch.join("\n")}"
+end
+
+def parseGitCommit(result,g)
+	# do nothing for the moment
+
+end
